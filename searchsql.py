f45a31a18e64597f4cc1eb47170c60cb0390785f
Ann Schoenenberger <schoenann@gmail.com>
2015-08-24 19:43:04 -0400

    Practice files for all sql commands

---
+++ searchsql.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+	c = connection.cursor()
+
+	c.execute("SELECT firstname, lastname from employees")
+	
+	rows = c.fetchall()
+	print rows
+
+	for r in rows:
+		print r[0], r[1]
--- sqlb.py
+++ sqlb.py
@@ -2,7 +2,11 @@ import sqlite3
 
 with sqlite3.connect("new.db") as conn:
 	curs = conn.cursor()
-	curs.execute("INSERT INTO population VALUES('New York City', \
-		'NY', 8200000)")
-	curs.execute("INSERT INTO population VALUES('San Francisco', \
-		'CA', 800000)")
+	try:
+
+	    curs.execute("INSERT INTO population VALUES('New York City', \
+		    'NY', 8200000)")
+	    curs.execute("INSERT INTO population VALUES('San Francisco', \
+		    'CA', 800000)")
+	except sqlite3.OperationalError:
+		print "Something went wrong. Try again..."
